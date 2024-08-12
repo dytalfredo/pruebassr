08f882c197c39b343ebe4c84ce4ab4f6e12d6b48
Alfredo <dytalfredomendoza@gmail.com>
2024-08-11 21:22:35 -0400

    Listo

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE ='Tech People Blog';
+export const SITE_DESCRIPTION ="Articulos, Historias Y Desarrollo";
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 3;
